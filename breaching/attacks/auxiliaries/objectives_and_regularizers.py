fix masking

breaching/attacks/auxiliaries/objectives_and_regularizers.py
@@ -53,9 +53,10 @@ class MaskedCosineSimilarity(torch.nn.Module):
         scalar_product, rec_norm, data_norm = 0.0, 0.0, 0.0
         for rec, data in zip(gradient_rec, gradient_data):
             mask = data.abs() > self.mask_value
-            scalar_product += (rec * data * self.mask_value).sum()
+            scalar_product += (rec * data * mask).sum()
             rec_norm += (rec * self.mask_value).pow(2).sum()
             data_norm += (data * self.mask_value).pow(2).sum()
+            breakpoint()
 
         objective = 1 - scalar_product / rec_norm.sqrt() / data_norm.sqrt()
 
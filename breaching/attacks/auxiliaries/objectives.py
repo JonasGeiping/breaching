c39de3ca8de7ae825480e996571fdd358c77665e
jonas Geiping <jonas.geiping@googlemail.com>
2021-12-03 13:23:25 -0500

    Objective variations

--- breaching/attacks/auxiliaries/objectives.py
+++ breaching/attacks/auxiliaries/objectives.py
@@ -8,6 +8,7 @@ from .make_functional import make_functional_with_buffers
 
 class GradientLoss(torch.nn.Module):
     """Super-class to simplify gradient-based objectives."""
+
     def __init__(self):
         super().__init__()
         self.task_regularization = 0
@@ -95,6 +96,32 @@ class Euclidean(GradientLoss):
 
         return objective
 
+
+class L1Loss(GradientLoss):
+    """Gradient matching based on the L1 distance of two gradient vectors."""
+
+    def __init__(self, scale=1.0, task_regularization=0.0, **kwargs):
+        super().__init__()
+        self.scale = scale
+        self.task_regularization = task_regularization
+
+    def gradient_based_loss(self, gradient_rec, gradient_data):
+        return self._l1loss(gradient_rec, gradient_data) * self.scale
+
+    def __repr__(self):
+        return f"L1 loss with scale={self.scale} and task reg={self.task_regularization}"
+
+    @staticmethod
+    @torch.jit.script
+    def _l1loss(gradient_rec: List[torch.Tensor], gradient_data: List[torch.Tensor]):
+        objective = gradient_rec[0].new_zeros(1,)
+        for rec, data in zip(gradient_rec, gradient_data):
+            objective += (rec - data).abs().sum()
+        return 0.5 * objective
+
+        return objective
+
+
 class CosineSimilarity(GradientLoss):
     """Gradient matching based on cosine similarity of two gradient vectors."""
 
@@ -121,9 +148,29 @@ class CosineSimilarity(GradientLoss):
             rec_norm += rec.pow(2).sum()
             data_norm += data.pow(2).sum()
 
-        # objective = torch.acos(scalar_product / rec_norm.sqrt() / data_norm.sqrt())
-        objective = 1 - scalar_product/ (rec_norm.sqrt()  * data_norm.sqrt())
+        objective = 1 - scalar_product / (rec_norm.sqrt() * data_norm.sqrt())
         return objective
+
+
+class AngularSimilarity(CosineSimilarity):
+    """Gradient matching based on angular similarity of two gradient vectors.
+
+    This is basically a more linear cosine similarity."""
+
+    def __init__(self, scale=1.0, task_regularization=0.0, fudge_factor=1e-7, **kwargs):
+        super().__init__()
+        self.scale = scale
+        self.task_regularization = task_regularization
+        self.fudge_factor = 1e-7
+
+    def gradient_based_loss(self, gradient_rec, gradient_data):
+        cosine = 1 - self._cosine_sim(gradient_rec, gradient_data)
+        angle = torch.acos(cosine.clamp(min=-1 + self.fudge_factor, max=1 - self.fudge_factor))
+
+        return angle / torch.pi * self.scale
+
+    def __repr__(self):
+        return f"Angular Similarity with scale={self.scale} and task reg={self.task_regularization}"
 
 
 class MaskedCosineSimilarity(GradientLoss):
@@ -153,29 +200,6 @@ class MaskedCosineSimilarity(GradientLoss):
         return objective * self.scale
 
 
-class ParameterCosineSimilarity(GradientLoss):
-    """Gradient matching based on cosine similarity of two gradient vectors.
-
-    In this variant all parameter blocks are matched separately."""
-
-    def __init__(self, scale=1.0, task_regularization=0.0, **kwargs):
-        super().__init__()
-        self.scale = scale
-        self.task_regularization = task_regularization
-
-    def __repr__(self):
-        return f"Parameter-wise Cosine Similarity with scale={self.scale} and task reg={self.task_regularization}"
-
-    def gradient_based_loss(self, gradient_rec, gradient_data):
-        similarities, counter = 0, 0
-        for rec, data in zip(gradient_rec, gradient_data):
-            similarities += torch.nn.functional.cosine_similarity(rec.view(-1), data.view(-1), dim=0, eps=1e-8)
-            counter += 1
-        objective = 1 - similarities / counter
-
-        return objective * self.scale
-
-
 class FastCosineSimilarity(GradientLoss):
     """Gradient matching based on cosine similarity of two gradient vectors.
     No gradient flows through the normalization."""
@@ -188,8 +212,8 @@ class FastCosineSimilarity(GradientLoss):
     def gradient_based_loss(self, gradient_rec, gradient_data):
         return self._cosine_sim(gradient_rec, gradient_data) * self.scale
 
-    @staticmethod
-    @torch.jit.script
+    @ staticmethod
+    @ torch.jit.script
     def _cosine_sim(gradient_rec: List[torch.Tensor], gradient_data: List[torch.Tensor]):
         scalar_product = gradient_rec[0].new_zeros(1,)
         rec_norm = gradient_rec[0].new_zeros(1,)
@@ -208,9 +232,11 @@ class FastCosineSimilarity(GradientLoss):
         return f"Fast Cosine Similarity with scale={self.scale} and task reg={self.task_regularization}"
 
 
-class PearlmutterLoss(torch.nn.Module):
+class PearlmutterEuclidean(torch.nn.Module):
     """Use a first-order approximation of \nabla_x \nabla_g instead of the correct autograd value."""
-    def __init__(self, scale=1.0, eps=1e-3, level_gradients=False, fudge_factor=1e-6, task_regularization=0.0, **kwargs):
+
+    def __init__(self, scale=1.0, eps=1e-3, level_gradients=False, fudge_factor=1e-6, task_regularization=0.0,
+                 implementation='forward', **kwargs):
         super().__init__()
         self.scale = scale
         self.task_regularization = task_regularization
@@ -218,6 +244,7 @@ class PearlmutterLoss(torch.nn.Module):
         self.eps = eps
         self.level_gradients = level_gradients
         self.fudge_factor = fudge_factor
+        self.implementation = implementation
 
     def initialize(self, loss_fn, cfg_impl, local_hyperparams=None):
         self.loss_fn = loss_fn
@@ -226,6 +253,16 @@ class PearlmutterLoss(torch.nn.Module):
             raise ValueError('This loss is only implemented for local gradients so far.')
 
         self.cfg_impl = cfg_impl
+        if self.implementation == 'forward':
+            self._forward_impl = self._forward_differences
+        elif self.implementation == 'backward':
+            self._forward_impl = self._backward_differences
+        elif self.implementation == 'central':
+            self._forward_impl = self._central_differences
+        elif self.implementation == 'upwind':
+            self._forward_impl = self._upwind_differences
+        else:
+            raise ValueError(f'Invalid finite difference implementation {self.implementation} given.')
 
     def __repr__(self):
         return (f"Pearlmutter-type Finite Differences Loss with scale={self.scale} and task reg={self.task_regularization}."
@@ -235,8 +272,18 @@ class PearlmutterLoss(torch.nn.Module):
     def forward(self, model, gradient_data, candidate, labels):
         """Run through model twice to approximate 2nd-order derivative on residual."""
         model.zero_grad()
-
+        # Keep original parameters
         original_parameters = [p.detach().clone() for p in model.parameters()]
+        # Compute derivative and accumulate into candidate.grad
+        objective_value, task_loss = self._forward_impl(model, gradient_data, candidate, labels)
+
+        # Unpatch model:# Want to do this faster, somehow
+        for param, original_param in zip(model.parameters(), original_parameters):
+            param.data.copy_(original_param)
+        return objective_value, task_loss
+
+    def _forward_differences(self, model, gradient_data, candidate, labels):
+        """Run through model twice to approximate 2nd-order derivative on residual."""
         with torch.autocast(candidate.device.type, enabled=self.cfg_impl.mixed_precision):
             task_loss = self.loss_fn(model(candidate), labels)
         # Compute both model gradients and candidate gradients
@@ -248,21 +295,111 @@ class PearlmutterLoss(torch.nn.Module):
 
         # Compute first-order gradients of objective
         objective_value, first_order_grad = self._compute_objective_and_first_order(candidate, gradients, gradient_data)
-        eps_n = self.eps / torch.stack([g.pow(2).sum() for g in gradients]).sum().sqrt()  # Adapts eps to different block strengths
+        # Adapts eps to different block strengths
+        eps_n = self.eps / torch.stack([g.pow(2).sum() for g in gradients]).sum().sqrt()
         # Patch model and compute loss at offset vector:
-        torch._foreach_add_(list(model.parameters()), first_order_grad, alpha=-eps_n)
+        torch._foreach_add_(list(model.parameters()), first_order_grad, alpha=eps_n)
         with torch.autocast(candidate.device.type, enabled=self.cfg_impl.mixed_precision):
             offset_task_loss = self.loss_fn(model(candidate), labels)
         dLv_dx, = torch.autograd.grad(offset_task_loss, (candidate,), create_graph=False)
 
         # Compute finite difference approximation
-        candidate.grad -= (dLv_dx - dLdx) / eps_n  * self.scale
+        candidate.grad += (dLv_dx - dLdx) / eps_n * self.scale
         # Add task loss
         candidate.grad += self.task_regularization * dLdx
-        # Unpatch model:
-        # torch._foreach_sub_(list(model.parameters()), first_order_grad, alpha=eps_n)
-        for param, original_param in zip(model.parameters(), original_parameters):
-            param.data.copy_(original_param)
+
+        return objective_value, task_loss
+
+    def _backward_differences(self, model, gradient_data, candidate, labels):
+        """Run through model twice to approximate 2nd-order derivative on residual."""
+        with torch.autocast(candidate.device.type, enabled=self.cfg_impl.mixed_precision):
+            task_loss = self.loss_fn(model(candidate), labels)
+        # Compute both model gradients and candidate gradients
+        *gradients, dLdx = torch.autograd.grad(task_loss, (*model.parameters(), candidate), create_graph=False)
+        if self.level_gradients:
+            # Only a good idea if normalize_gradients=True
+            grad_norm = torch.stack([g.pow(2).sum() for g in gradients]).sum().sqrt()
+            torch._foreach_div_(gradients, max(grad_norm, self.fudge_factor))
+
+        # Compute first-order gradients of objective
+        objective_value, first_order_grad = self._compute_objective_and_first_order(candidate, gradients, gradient_data)
+        # Adapts eps to different block strengths
+        eps_n = self.eps / torch.stack([g.pow(2).sum() for g in gradients]).sum().sqrt()
+        # Patch model and compute loss at offset vector:
+        torch._foreach_sub_(list(model.parameters()), first_order_grad, alpha=eps_n)
+        with torch.autocast(candidate.device.type, enabled=self.cfg_impl.mixed_precision):
+            offset_task_loss = self.loss_fn(model(candidate), labels)
+        dLv_dx, = torch.autograd.grad(offset_task_loss, (candidate,), create_graph=False)
+
+        # Compute finite difference approximation
+        candidate.grad += (dLdx - dLv_dx) / eps_n * self.scale
+        # Add task loss
+        candidate.grad += self.task_regularization * dLdx
+
+        return objective_value, task_loss
+
+    def _central_differences(self, model, gradient_data, candidate, labels):
+        """Run through model twice to approximate 2nd-order derivative on residual."""
+        with torch.autocast(candidate.device.type, enabled=self.cfg_impl.mixed_precision):
+            task_loss = self.loss_fn(model(candidate), labels)
+        # Compute both model gradients and candidate gradients
+        gradients = torch.autograd.grad(task_loss, model.parameters(), create_graph=False)
+
+        # Compute first-order gradients of objective
+        objective_value, first_order_grad = self._compute_objective_and_first_order(candidate, gradients, gradient_data)
+        # Adapts eps to different block strengths
+        eps_n = self.eps / torch.stack([g.pow(2).sum() for g in gradients]).sum().sqrt()
+
+        # Patch model and compute loss at offset vectors:
+        torch._foreach_add_(list(model.parameters()), first_order_grad, alpha=0.5 * eps_n)
+        with torch.autocast(candidate.device.type, enabled=self.cfg_impl.mixed_precision):
+            offset_plus = self.loss_fn(model(candidate), labels)
+        dLvp_dx, = torch.autograd.grad(offset_plus, (candidate,), create_graph=False)
+
+        torch._foreach_sub_(list(model.parameters()), first_order_grad, alpha=eps_n)
+        with torch.autocast(candidate.device.type, enabled=self.cfg_impl.mixed_precision):
+            offset_minus = self.loss_fn(model(candidate), labels)
+        dLvm_dx, = torch.autograd.grad(offset_minus, (candidate,), create_graph=False)
+
+        # Compute finite difference approximation
+        candidate.grad += (dLvp_dx - dLvm_dx) / eps_n * self.scale
+        # Add task loss
+        candidate.grad += self.task_regularization * dLdx
+
+        return objective_value, task_loss
+
+    def _upwind_differences(self, model, gradient_data, candidate, labels):
+        """Run through model twice to approximate 2nd-order derivative on residual."""
+        with torch.autocast(candidate.device.type, enabled=self.cfg_impl.mixed_precision):
+            task_loss = self.loss_fn(model(candidate), labels)
+        # Compute both model gradients and candidate gradients
+        *gradients, dLdx = torch.autograd.grad(task_loss, (*model.parameters(), candidate), create_graph=False)
+
+        # Compute first-order gradients of objective
+        objective_value, first_order_grad = self._compute_objective_and_first_order(candidate, gradients, gradient_data)
+        # Adapts eps to different block strengths
+        eps_n = self.eps / torch.stack([g.pow(2).sum() for g in gradients]).sum().sqrt()
+
+        # Patch model and compute loss at offset vectors:
+        torch._foreach_add_(list(model.parameters()), first_order_grad, alpha=0.5 * eps_n)
+        with torch.autocast(candidate.device.type, enabled=self.cfg_impl.mixed_precision):
+            offset_plus = self.loss_fn(model(candidate), labels)
+        dLvp_dx, = torch.autograd.grad(offset_plus, (candidate,), create_graph=False)
+
+        torch._foreach_sub_(list(model.parameters()), first_order_grad, alpha=eps_n)
+        with torch.autocast(candidate.device.type, enabled=self.cfg_impl.mixed_precision):
+            offset_minus = self.loss_fn(model(candidate), labels)
+        dLvm_dx, = torch.autograd.grad(offset_minus, (candidate,), create_graph=False)
+
+        # Compute both finite differences
+        Dp = (dLvp_dx - dLdx) / eps_n
+        Dm = (dLdx - dLvm_dx) / eps_n
+        # Upwind based on dLdx
+        # Compute finite difference approximation
+        candidate.grad += (torch.max(dLdx, 0) * Dm + torch.min(dLdx, 0) * Dp) * self.scale
+        # Add task loss
+        candidate.grad += self.task_regularization * dLdx
+
         return objective_value, task_loss
 
     def _compute_objective_and_first_order(self, candidate, gradients, gradient_data):
@@ -275,7 +412,7 @@ class PearlmutterLoss(torch.nn.Module):
         return objective_value, residuals
 
 
-class PearlmutterCosine(PearlmutterLoss):
+class PearlmutterCosine(PearlmutterEuclidean):
     """Use a first-order approximation of \nabla_x \nabla_g instead of the correct autograd value."""
 
     def _compute_objective_and_first_order(self, candidate, gradients, gradient_data):
@@ -283,13 +420,7 @@ class PearlmutterCosine(PearlmutterLoss):
             scalar_product, grad_norm, data_norm = self._cosine_sim_components(gradients, gradient_data)
 
         first_order_cosine = torch._foreach_div(gradient_data, grad_norm * data_norm)
-        torch._foreach_sub_(first_order_cosine, gradients, alpha=scalar_product / (grad_norm.pow(3) * data_norm)) 
-        # first_order_cosine = torch._foreach_sub(gradient_data, gradients, alpha=scalar_product / grad_norm.pow(2))
-        #  torch._foreach_div_(first_order_cosine, -(grad_norm * data_norm))
-        # first_order_cosine = torch._foreach_mul(gradient_data, -grad_norm.pow(2))
-        # torch._foreach_sub_(first_order_cosine, gradients, alpha=-scalar_product)
-        # torch._foreach_div_(first_order_cosine, grad_norm.pow(3) * data_norm.pow(2))
-        
+        torch._foreach_sub_(first_order_cosine, gradients, alpha=scalar_product / (grad_norm.pow(3) * data_norm))
 
         objective_value = self.scale * (1 - scalar_product / (grad_norm * data_norm))
         return objective_value, first_order_cosine
@@ -309,13 +440,12 @@ class PearlmutterCosine(PearlmutterLoss):
         return scalar_product, rec_norm.sqrt(), data_norm.sqrt()
 
 
-
-
 objective_lookup = {'euclidean': Euclidean,
                     'cosine-similarity': CosineSimilarity,
                     'masked-cosine-similarity': MaskedCosineSimilarity,
-                    'paramwise-cosine-similarity': ParameterCosineSimilarity,
                     'fast-cosine-similarity': FastCosineSimilarity,
-                    'pearlmutter-loss': PearlmutterLoss,
+                    'angular': AngularSimilarity,
+                    'l1': L1Loss,
+                    'pearlmutter-loss': PearlmutterEuclidean,
                     'pearlmutter-cosine': PearlmutterCosine,
                     }
